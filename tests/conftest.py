3bec8c6cb43bcc9c510ab7d5d5a6aa4523901bf8
Loren Gordon <lorengordon@users.noreply.github.com>
2017-07-11 18:40:52 -0400

    Adds ami tests using testinfra

---
+++ tests/conftest.py
@@ -0,0 +1,40 @@
+import os
+
+import distro
+import pytest
+from six.moves import urllib
+
+# Static globals
+METADATA_KERNEL = 'http://169.254.169.254/latest/meta-data/kernel-id'
+FIPS_DISABLED = set(['true', 'TRUE', '1', 'on'])
+
+# Markers
+VIRTUALIZATION_MARKERS = set('hvm paravirutal'.split())
+PLAT_MARKERS = set('el6 el7'.split())
+FIPS_MARKERS = set('fips_enabled fips_disabled'.split())
+
+# Platform-specific globals
+PLAT = 'el' + distro.major_version()
+FIPS = 'fips_disabled' if os.environ.get('SPEL_DISABLEFIPS') in FIPS_DISABLED \
+    else 'fips_enabled'
+VIRT = 'hvm'
+try:
+    urllib.request.urlopen(METADATA_KERNEL)
+    VIRT = 'paravirtual'
+except urllib.error.URLError:
+    pass
+
+
+def pytest_runtest_setup(item):
+    if isinstance(item, item.Function):
+        if not item.get_marker(PLAT):
+            if PLAT_MARKERS.intersection(item.keywords):
+                pytest.skip('does not run on platform {0}'.format(PLAT))
+        if not item.get_marker(VIRT):
+            if VIRTUALIZATION_MARKERS.intersection(item.keywords):
+                pytest.skip(
+                    'does not run on virtualization type {0}'.format(VIRT))
+        if not item.get_marker(FIPS):
+            if FIPS_MARKERS.intersection(item.keywords):
+                pytest.skip(
+                    'test incompatible with fips mode, {0}'.format(FIPS))
---
+++ tests/test_ami.py
@@ -0,0 +1,104 @@
+import pytest
+
+
+@pytest.mark.hvm
+def test_10_gigabit(host):
+    interface = host.interface('eth0')
+    assert interface.speed == 10000
+
+
+def test_root_volume_is_resized(host):
+    cmd = 'test $(vgs --noheadings -o pv_free | sed \'s/ //g\') != 0'
+    pv_free = host.run(cmd)
+    assert pv_free.exit_status == 0
+
+
+@pytest.mark.parametrize("name", [
+    ("aws-amitools-ec2"),
+    ("aws-apitools-as"),
+    ("aws-apitools-cfn"),
+    ("aws-apitools-common"),
+    ("aws-apitools-ec2"),
+    ("aws-apitools-elb"),
+    ("aws-apitools-iam"),
+    ("aws-apitools-mon"),
+    ("aws-apitools-rds"),
+    ("aws-cfn-bootstrap")
+])
+def test_common_aws_pkgs(host, name):
+    pkg = host.package(name)
+    assert pkg.is_installed
+
+
+@pytest.mark.el6
+@pytest.mark.parametrize("name", [
+    ("aws-vpc-nat")
+])
+def test_el6_aws_pkgs(host, name):
+    pkg = host.package(name)
+    assert pkg.is_installed
+
+
+@pytest.mark.el7
+@pytest.mark.parametrize("name", [
+    ("aws-scripts-ses")
+])
+def test_el7_aws_pkgs(host, name):
+    pkg = host.package(name)
+    assert pkg.is_installed
+
+
+def test_aws_cli_is_in_path(host):
+    assert host.exists('aws')
+
+
+def test_repo_access(host):
+    cmd = 'yum repolist all 2> /dev/null | sed -n \'/^repo id/,$p\''
+    repos = host.run(cmd)
+    assert repos.exit_status == 0
+
+
+def test_boot_is_mounted(host):
+    boot = host.mount_point('/boot')
+    assert boot.exists
+
+
+def test_tmp_mount_properties(host):
+    tmp = host.mount_point('/tmp')
+    assert tmp.exists
+    assert tmp.device == 'tmpfs'
+    assert tmp.filesystem == 'tmpfs'
+
+
+@pytest.mark.el6
+def test_el6_selinux_permissive(host):
+    cmd = 'test $(getenforce) = \'Permissive\''
+    selinux_permissive = host.run(cmd)
+    assert selinux_permissive.exit_status == 0
+
+
+@pytest.mark.el7
+def test_el7_selinux_enforcing(host):
+    cmd = 'test $(getenforce) = \'Enforcing\''
+    selinux_permissive = host.run(cmd)
+    assert selinux_permissive.exit_status == 0
+
+
+@pytest.mark.el7
+@pytest.mark.fips_enabled
+def test_el7_fips_enabled(host):
+    fips = host.file('/proc/sys/crypto/fips_enabled')
+    assert fips.exists and fips.content.strip() == b'1'
+
+
+@pytest.mark.el7
+@pytest.mark.fips_disabled
+def test_el7_fips_disabled(host):
+    fips = host.file('/proc/sys/crypto/fips_enabled')
+    assert not fips.exists or fips.content.strip() == b'0'
+
+
+@pytest.mark.el6
+def test_el6_xen_root_dev_mapping(host):
+    grub = host.file('/boot/grub/grub.conf')
+    assert grub.contains('xen_blk')
