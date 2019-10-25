6a1632cf1ecde2ea609a08a41f3d5e610836b650
Loren Gordon <lorengordon@users.noreply.github.com>
2019-10-25 08:57:04 -0400

    Reorganizes DEFAULTREPOS a bit to explain all the entries

--- spel/scripts/amigen7-build.sh
+++ spel/scripts/amigen7-build.sh
@@ -32,23 +32,30 @@ DEFAULTREPOS=(
     base
     updates
     extras
-    epel
 )
 if [[ $(rpm --quiet -q redhat-release-server)$? -eq 0 ]]
 then
     DEFAULTREPOS=(
+        # RHUI 2 repo names
+        rhui-REGION-client-config-server-7
+        rhui-REGION-rhel-server-releases
+        rhui-REGION-rhel-server-rh-common
+        rhui-REGION-rhel-server-optional
+        rhui-REGION-rhel-server-extras
+        # RHUI 3 repo names
         rhui-client-config-server-7
         rhel-7-server-rhui-rpms
         rhel-7-server-rhui-rh-common-rpms
         rhel-7-server-rhui-optional-rpms
         rhel-7-server-rhui-extras-rpms
+        # RHUI 3 repo names in EL7.7 GA
         rhui-rhel-7-server-rhui-rpms
         rhui-rhel-7-server-rhui-rh-common-rpms
         rhui-rhel-7-server-rhui-optional-rpms
         rhui-rhel-7-server-rhui-extras-rpms
-        epel
     )
 fi
+DEFAULTREPOS+=(epel)
 
 if [[ -z "${CUSTOMREPONAME}" ]]
 then
