83279278761b935b06f89409ea2975c744b1952a
Loren Gordon <lorengordon@users.noreply.github.com>
2021-01-22 09:31:32 -0800

    Updates vagrant automation from Ubuntu 18.04 to 20.04

--- build/vagrant/infra/spel-vagrant.sh
+++ build/vagrant/infra/spel-vagrant.sh
@@ -13,15 +13,31 @@ SPEL_VERSION=${spel_version}
 SSM_VAGRANTCLOUD_TOKEN=${ssm_vagrantcloud_token}
 VAGRANTCLOUD_USER=${vagrantcloud_user}
 
-# update machine
-sleep 120
+# internal vars
+CLONE_DIR=/tmp/spel
+S3_BUCKET="s3://$${ARTIFACT_LOCATION}"
 
+if [ "$${SPEL_CI}" = "true" ]
+then
+    # artifacts for CI build will be stored in /ci
+    # CI build will skip vagrant-cloud post-provisioner
+    EXCEPT_STEP="vagrant-cloud"
+    S3_BUCKET="$${S3_BUCKET}/ci"
+
+    export EXCEPT_STEP
+    export S3_BUCKET
+fi
+
+# update PATH
+export PATH="$${HOME}/.local/bin:$${PATH}"
+
+# update machine
+/usr/bin/cloud-init status --wait
 sudo apt-get update && sudo apt-get install -y \
-awscli \
-vagrant \
-python3-pip \
-virtualbox \
-virtualbox-guest-additions-iso
+    vagrant \
+    python3-pip \
+    virtualbox \
+    virtualbox-guest-additions-iso
 
 pip3 install awscli --upgrade --user
 
@@ -33,9 +49,8 @@ sudo mv packer /usr/local/bin/
 packer version
 
 # download spel
-cd /tmp
-git clone "$${CODE_REPO}"
-cd spel
+git clone "$${CODE_REPO}" "$${CLONE_DIR}"
+cd "$${CLONE_DIR}"
 
 if [[ -n "$${SOURCE_COMMIT}" ]] ; then
     # decide whether to switch to pull request or a branch
@@ -46,29 +61,13 @@ if [[ -n "$${SOURCE_COMMIT}" ]] ; then
     git checkout "$${SOURCE_COMMIT}"
 fi
 
-pwd
-
-# setup environment
+# build vagrant box
 VAGRANTCLOUD_TOKEN=$(aws ssm get-parameter --name "$${SSM_VAGRANTCLOUD_TOKEN}" --with-decryption --query Parameter.Value --out text --region us-east-1)
 export VAGRANTCLOUD_TOKEN="$${VAGRANTCLOUD_TOKEN}"
 
-# build vagrant box
-mkdir -p "/tmp/spel/.spel/$${SPEL_VERSION}/"
+mkdir -p "$${CLONE_DIR}/.spel/$${SPEL_VERSION}/"
 export PACKER_LOG=1
-export PACKER_LOG_PATH="/tmp/spel/.spel/$${SPEL_VERSION}/packer.log"
-
-# setup s3 bucket to store artifacts
-# artifacts for CI build will be stored in /ci
-S3_BUCKET="s3://$${ARTIFACT_LOCATION}"
-
-if [ "$${SPEL_CI}" = "true" ]
-then
-    EXCEPT_STEP="vagrant-cloud"
-    S3_BUCKET="$${S3_BUCKET}/ci"
-
-    export EXCEPT_STEP
-    export S3_BUCKET
-fi
+export PACKER_LOG_PATH="$${CLONE_DIR}/.spel/$${SPEL_VERSION}/packer.log"
 
 packer build \
   -var "iso_url_centos7=$${ISO_URL_CENTOS7}" \
@@ -80,7 +79,7 @@ packer build \
   spel/minimal-linux.json
 
 # remove .ova and .box files from artifact location
-find /tmp/spel/ -type f \( -name '*.box' -o -name '*.ova' \) -print0 | xargs -0 rm -f
+find "$${CLONE_DIR}/" -type f \( -name '*.box' -o -name '*.ova' \) -print0 | xargs -0 rm -f
 
 # upload artifacts to S3
-aws s3 cp --recursive /tmp/spel/.spel/ "$${S3_BUCKET}/"
+aws s3 cp --recursive "$${CLONE_DIR}/.spel/" "$${S3_BUCKET}/"
