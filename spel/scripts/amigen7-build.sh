4118b11b942e77eddbfeeaa8bcb081946ffd67b6
Owen Blandford <oblandford@novetta.com>
2021-01-27 09:21:55 -0500

    Adding the Error handler function from the amigen8 script. This function is currently not called in this script and should have no impact but we would like to use it with future changes.

--- spel/scripts/amigen7-build.sh
+++ spel/scripts/amigen7-build.sh
@@ -4,6 +4,7 @@
 # task.
 #
 ##############################################################################
+PROGNAME="$(basename "$0")"
 AMIGENSOURCE="${SPEL_AMIGENSOURCE:-https://github.com/plus3it/AMIgen7.git}"
 AMIGENBRANCH="${SPEL_AMIGENBRANCH:-master}"
 AMIGENMANFST="${SPEL_AMIGENMANFST}"
@@ -26,6 +27,39 @@ FIPSDISABLE="${SPEL_FIPSDISABLE}"
 VGNAME="${SPEL_VGNAME:-VolGroup00}"
 
 read -r -a BUILDDEPS <<< "${SPEL_BUILDDEPS:-lvm2 parted yum-utils unzip git}"
+
+
+# Make interactive-execution more-verbose unless explicitly told not to
+if [[ $( tty -s ) -eq 0 ]] && [[ ${DEBUG} == "UNDEF" ]]
+then
+   DEBUG="true"
+fi
+
+
+# Error handler function
+function err_exit {
+   local ERRSTR
+   local ISNUM
+   local SCRIPTEXIT
+
+   ERRSTR="${1}"
+   ISNUM='^[0-9]+$'
+   SCRIPTEXIT="${2:-1}"
+
+   if [[ ${DEBUG} == true ]]
+   then
+      # Our output channels
+      logger -i -t "${PROGNAME}" -p kern.crit -s -- "${ERRSTR}"
+   else
+      logger -i -t "${PROGNAME}" -p kern.crit -- "${ERRSTR}"
+   fi
+
+   # Only exit if requested exit is numerical
+   if [[ ${SCRIPTEXIT} =~ ${ISNUM} ]]
+   then
+      exit "${SCRIPTEXIT}"
+   fi
+}
 
 PYTHON3_BIN="/usr/bin/python3.6"
 PYTHON3_LINK="/usr/bin/python3"
