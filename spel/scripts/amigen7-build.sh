9bb00bc00ae0f10377687274d12689ef6db7091d
Owen Blandford <oblandford@novetta.com>
2021-04-09 14:31:41 -0400

    added suggested changes to the PR

--- spel/scripts/amigen7-build.sh
+++ spel/scripts/amigen7-build.sh
@@ -10,7 +10,6 @@ AMIGENMANFST="${SPEL_AMIGENMANFST}"
 AMIGENPKGGRP="${SPEL_AMIGENPKGGRP:-core}"
 AMIGENSOURCE="${SPEL_AMIGENSOURCE:-https://github.com/plus3it/AMIgen7.git}"
 AMIGENSTORLAY="${SPEL_AMIGENSTORLAY:-/:rootVol:4,swap:swapVol:2,/home:homeVol:1,/var:varVol:2,/var/log:logVol:2,/var/log/audit:auditVol:100%FREE}"
-AMIUTILS="/tmp/ami-utils"
 AMIUTILSSOURCE="${SPEL_AMIUTILSSOURCE:-https://github.com/ferricoxide/Lx-GetAMI-Utils.git}"
 AWSCLIV1SOURCE="${SPEL_AWSCLIV1SOURCE:-https://s3.amazonaws.com/aws-cli/awscli-bundle.zip}"
 AWSCLIV2SOURCE="${SPEL_AWSCLIV2SOURCE}"
@@ -21,17 +20,18 @@ CLOUDPROVIDER="${SPEL_CLOUDPROVIDER:-aws}"
 CUSTOMREPONAME="${SPEL_CUSTOMREPONAME}"
 CUSTOMREPORPM="${SPEL_CUSTOMREPORPM}"
 DEVNODE="${SPEL_DEVNODE:-/dev/nvme0n1}"
-ELBUILD="/tmp/el-build"
 EPELRELEASE="${SPEL_EPELRELEASE:-https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm}"
 EPELREPO="${SPEL_EPELREPO:-epel}"
 EXTRARPMS="${SPEL_EXTRARPMS}"
 FIPSDISABLE="${SPEL_FIPSDISABLE}"
-PYTHON3_BIN="/usr/bin/python3.6"
-PYTHON3_LINK="/usr/bin/python3"
 VGNAME="${SPEL_VGNAME:-VolGroup00}"
 
 read -r -a BUILDDEPS <<< "${SPEL_BUILDDEPS:-lvm2 parted yum-utils unzip git}"
 
+AMIUTILS="/tmp/ami-utils"
+ELBUILD="/tmp/el-build"
+PYTHON3_BIN="/usr/bin/python3.6"
+PYTHON3_LINK="/usr/bin/python3"
 
 # Make interactive-execution more-verbose unless explicitly told not to
 if [[ $( tty -s ) -eq 0 ]] && [[ ${DEBUG} == "UNDEF" ]]
@@ -186,38 +186,38 @@ disable_strict_host_check()
 
 # Changing this to a function to better align with amigen8-build
 function CollectManifest {
-echo "Saving the release info to the manifest"
-cat "${CHROOT}/etc/redhat-release" > /tmp/manifest.txt
-
-if [[ "${CLOUDPROVIDER}" == "aws" ]]
-then
-    if [[ -n "$AWSCLIV1SOURCE" ]]
+    echo "Saving the release info to the manifest"
+    cat "${CHROOT}/etc/redhat-release" > /tmp/manifest.txt
+    
+    if [[ "${CLOUDPROVIDER}" == "aws" ]]
     then
-        echo "Saving the aws-cli-v1 version to the manifest"
+        if [[ -n "$AWSCLIV1SOURCE" ]]
+        then
+            echo "Saving the aws-cli-v1 version to the manifest"
+            [[ -o xtrace ]] && XTRACE='set -x' || XTRACE='set +x'
+            set +x
+            (chroot "${CHROOT}" /usr/local/bin/aws1 --version) 2>&1 | tee -a /tmp/manifest.txt
+            eval "$XTRACE"
+        fi
+        if [[ -n "$AWSCLIV2SOURCE" ]]
+        then
+            echo "Saving the aws-cli-v2 version to the manifest"
+            [[ -o xtrace ]] && XTRACE='set -x' || XTRACE='set +x'
+            set +x
+            (chroot "${CHROOT}" /usr/local/bin/aws2 --version) 2>&1 | tee -a /tmp/manifest.txt
+            eval "$XTRACE"
+        fi
+    elif [[ "${CLOUDPROVIDER}" == "azure" ]]
+    then
+        echo "Saving the waagent version to the manifest"
         [[ -o xtrace ]] && XTRACE='set -x' || XTRACE='set +x'
         set +x
-        (chroot "${CHROOT}" /usr/local/bin/aws1 --version) 2>&1 | tee -a /tmp/manifest.txt
+        (chroot "${CHROOT}" /usr/sbin/waagent --version) 2>&1 | tee -a /tmp/manifest.txt
         eval "$XTRACE"
     fi
-    if [[ -n "$AWSCLIV2SOURCE" ]]
-    then
-        echo "Saving the aws-cli-v2 version to the manifest"
-        [[ -o xtrace ]] && XTRACE='set -x' || XTRACE='set +x'
-        set +x
-        (chroot "${CHROOT}" /usr/local/bin/aws2 --version) 2>&1 | tee -a /tmp/manifest.txt
-        eval "$XTRACE"
-    fi
-elif [[ "${CLOUDPROVIDER}" == "azure" ]]
-then
-    echo "Saving the waagent version to the manifest"
-    [[ -o xtrace ]] && XTRACE='set -x' || XTRACE='set +x'
-    set +x
-    (chroot "${CHROOT}" /usr/sbin/waagent --version) 2>&1 | tee -a /tmp/manifest.txt
-    eval "$XTRACE"
-fi
-
-echo "Saving the RPM manifest"
-rpm --root "${CHROOT}" -qa | sort -u >> /tmp/manifest.txt
+    
+    echo "Saving the RPM manifest"
+    rpm --root "${CHROOT}" -qa | sort -u >> /tmp/manifest.txt
 }
 
 
