345e0ee1005facfc62297b417de24d3eb004afab
Owen Blandford <oblandford@novetta.com>
2021-04-09 08:50:18 -0400

    Cosmetic change moving variable definitions

--- spel/scripts/amigen7-build.sh
+++ spel/scripts/amigen7-build.sh
@@ -25,13 +25,13 @@ EPELREPO="${SPEL_EPELREPO:-epel}"
 EXTRARPMS="${SPEL_EXTRARPMS}"
 FIPSDISABLE="${SPEL_FIPSDISABLE}"
 VGNAME="${SPEL_VGNAME:-VolGroup00}"
-
-read -r -a BUILDDEPS <<< "${SPEL_BUILDDEPS:-lvm2 parted yum-utils unzip git}"
-
 PYTHON3_BIN="/usr/bin/python3.6"
 PYTHON3_LINK="/usr/bin/python3"
 ELBUILD="/tmp/el-build"
 AMIUTILS="/tmp/ami-utils"
+
+read -r -a BUILDDEPS <<< "${SPEL_BUILDDEPS:-lvm2 parted yum-utils unzip git}"
+
 
 # Make interactive-execution more-verbose unless explicitly told not to
 if [[ $( tty -s ) -eq 0 ]] && [[ ${DEBUG} == "UNDEF" ]]
