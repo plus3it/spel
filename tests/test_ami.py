9b6a8096f393bb119c2000ec6a08661a61c7770a
Loren Gordon <lorengordon@users.noreply.github.com>
2021-12-14 15:42:18 -0800

    Continues using amigen8 feature to install ssm-agent
    This feature supports enabling the service, in addition to the install

--- tests/test_ami.py
+++ tests/test_ami.py
@@ -159,6 +159,8 @@ def test_var_run_symlink(host):
 @pytest.mark.el7
 @pytest.mark.parametrize("service", [
     ("autotune.service"),
+    ("ec2-instance-connect.service"),
+    ("hibinit-agent.service"),
 ])
 def test_el7_systemd_services(host, service):
     chk_service = host.service(service)
@@ -167,8 +169,6 @@ def test_el7_systemd_services(host, service):
 
 @pytest.mark.parametrize("service", [
     ("amazon-ssm-agent.service"),
-    ("ec2-instance-connect.service"),
-    ("hibinit-agent.service"),
 ])
 def test_systemd_services(host, service):
     chk_service = host.service(service)
