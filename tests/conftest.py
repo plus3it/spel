2889cbe0ef60fc2e6bb486d227d8786652f256c5
Loren Gordon <lorengordon@users.noreply.github.com>
2022-01-21 07:53:20 -0800

    Blacks the python files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,43 +1,44 @@
+"""Configuration for pytest."""
 import os
+import urllib.error
+import urllib.request
 
 import distro
 import pytest
-import urllib.request, urllib.error
 
 # Static globals
-METADATA_KERNEL = 'http://169.254.169.254/latest/meta-data/kernel-id'
-FIPS_DISABLED = set(['true', 'TRUE', '1', 'on'])
+METADATA_KERNEL = "http://169.254.169.254/latest/meta-data/kernel-id"
+FIPS_DISABLED = set(["true", "TRUE", "1", "on"])
 
 # Markers
-VIRTUALIZATION_MARKERS = set(['hvm', 'paravirtual'])
-PLAT_MARKERS = set(['el7', 'el8'])
-FIPS_MARKERS = set(['fips_enabled', 'fips_disabled'])
-AMIUTILS_MARKERS = set(['amiutils_enabled', 'amiutils_disabled'])
+VIRTUALIZATION_MARKERS = set(["hvm", "paravirtual"])
+PLAT_MARKERS = set(["el7", "el8"])
+FIPS_MARKERS = set(["fips_enabled", "fips_disabled"])
+AMIUTILS_MARKERS = set(["amiutils_enabled", "amiutils_disabled"])
 
 # Platform-specific globals
-PLAT = 'el' + distro.major_version()
-FIPS = 'fips_disabled' if os.environ.get('SPEL_DISABLEFIPS') in FIPS_DISABLED \
-    else 'fips_enabled'
-AMIUTILS = 'amiutils_enabled' if os.environ.get('SPEL_AMIUTILSOURCE') \
-    else 'amiutils_disabled'
-VIRT = 'hvm'
+PLAT = "el" + distro.major_version()
+FIPS = (
+    "fips_disabled"
+    if os.environ.get("SPEL_DISABLEFIPS") in FIPS_DISABLED
+    else "fips_enabled"
+)
+AMIUTILS = (
+    "amiutils_enabled" if os.environ.get("SPEL_AMIUTILSOURCE") else "amiutils_disabled"
+)
+VIRT = "hvm"
 try:
-    urllib.request.urlopen(METADATA_KERNEL)
-    VIRT = 'paravirtual'
+    with urllib.request.urlopen(METADATA_KERNEL):
+        VIRT = "paravirtual"
 except urllib.error.URLError:
     pass
 
 
 def pytest_configure(config):
-    config.addinivalue_line(
-        "markers", "el7: mark test to run only on el7 platforms"
-    )
-    config.addinivalue_line(
-        "markers", "el8: mark test to run only on el8 platforms"
-    )
-    config.addinivalue_line(
-        "markers", "hvm: mark test to run only on hvm instances"
-    )
+    """Configure pytest."""
+    config.addinivalue_line("markers", "el7: mark test to run only on el7 platforms")
+    config.addinivalue_line("markers", "el8: mark test to run only on el8 platforms")
+    config.addinivalue_line("markers", "hvm: mark test to run only on hvm instances")
     config.addinivalue_line(
         "markers", "paravirtual: mark test to run only on paravirtual instances"
     )
@@ -51,28 +52,28 @@ def pytest_configure(config):
         "markers", "amiutils_enabled: mark test to run only if AMI Utils pkgs were used"
     )
     config.addinivalue_line(
-        "markers", "amiutils_disabled: mark test to run only if AMI Utils pkgs were not used"
+        "markers",
+        "amiutils_disabled: mark test to run only if AMI Utils pkgs were not used",
     )
 
 
 def pytest_runtest_setup(item):
+    """Configure pytest."""
     if isinstance(item, pytest.Function):
         if not item.get_closest_marker(PLAT):
             if PLAT_MARKERS.intersection(item.keywords):
-                pytest.skip('does not run on platform {0}'.format(PLAT))
+                pytest.skip(f"does not run on platform {PLAT}")
         if not item.get_closest_marker(VIRT):
             if VIRTUALIZATION_MARKERS.intersection(item.keywords):
-                pytest.skip(
-                    'does not run on virtualization type {0}'.format(VIRT))
+                pytest.skip(f"does not run on virtualization type {VIRT}")
         if not item.get_closest_marker(FIPS):
             if FIPS_MARKERS.intersection(item.keywords):
-                pytest.skip(
-                    'test incompatible with fips mode, {0}'.format(FIPS))
+                pytest.skip(f"test incompatible with fips mode, {FIPS}")
         if not item.get_closest_marker(AMIUTILS_MARKERS):
             if AMIUTILS_MARKERS.intersection(item.keywords):
-                pytest.skip(
-                    'does not run when ami utils is deselected, {0}'.format(AMIUTILS))
+                pytest.skip(f"does not run when ami utils is deselected, {AMIUTILS}")
 
 
-def pytest_logger_stdoutloggers(item):
-    return ['spel_validation']
+def pytest_logger_stdoutloggers(item):  # pylint: disable=unused-argument
+    """Configure pytest logger."""
+    return ["spel_validation"]
--- tests/test_ami.py
+++ tests/test_ami.py
@@ -1,137 +1,143 @@
+"""Test spel aws ami builds."""
+# pylint: disable=missing-function-docstring
 import logging
 
 import pytest
 
-log = logging.getLogger('spel_validation')
+log = logging.getLogger("spel_validation")
 log.setLevel(logging.INFO)
 
 
-def test_root_volume_is_resized(host):
-    cmd = 'test $(vgs --noheadings -o pv_free | sed \'s/ //g\') != 0'
+def test_root_volume_is_resized(host):  # noqa: D103
+    cmd = "test $(vgs --noheadings -o pv_free | sed 's/ //g') != 0"
     pv_free = host.run(cmd)
     assert pv_free.exit_status == 0
 
 
 @pytest.mark.amiutils_enabled
-@pytest.mark.parametrize("name", [
-])
-def test_common_amiutils_pkgs(host, name):
+@pytest.mark.parametrize("name", [])
+def test_common_amiutils_pkgs(host, name):  # noqa: D103
     pkg = host.package(name)
     if pkg.is_installed:
         log.info(
-            '%s',
-            {'pkg': pkg.name, 'version': pkg.version, 'release': pkg.release})
+            "%s", {"pkg": pkg.name, "version": pkg.version, "release": pkg.release}
+        )
     assert pkg.is_installed
 
 
 @pytest.mark.amiutils_enabled
 @pytest.mark.el7
-@pytest.mark.parametrize("name", [
-    ("aws-amitools-ec2"),
-    ("aws-apitools-as"),
-    ("aws-apitools-cfn"),
-    ("aws-apitools-common"),
-    ("aws-apitools-ec2"),
-    ("aws-apitools-elb"),
-    ("aws-apitools-iam"),
-    ("aws-apitools-mon"),
-    ("aws-apitools-rds"),
-    ("aws-cfn-bootstrap"),
-    ("aws-scripts-ses")
-])
-def test_el7_amiutils_pkgs(host, name):
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("aws-amitools-ec2"),
+        ("aws-apitools-as"),
+        ("aws-apitools-cfn"),
+        ("aws-apitools-common"),
+        ("aws-apitools-ec2"),
+        ("aws-apitools-elb"),
+        ("aws-apitools-iam"),
+        ("aws-apitools-mon"),
+        ("aws-apitools-rds"),
+        ("aws-cfn-bootstrap"),
+        ("aws-scripts-ses"),
+    ],
+)
+def test_el7_amiutils_pkgs(host, name):  # noqa: D103
     pkg = host.package(name)
     if pkg.is_installed:
         log.info(
-            '%s',
-            {'pkg': pkg.name, 'version': pkg.version, 'release': pkg.release})
+            "%s", {"pkg": pkg.name, "version": pkg.version, "release": pkg.release}
+        )
     assert pkg.is_installed
 
 
-def test_aws_cli_is_in_path(host):
-    cmd = 'aws --version'
+def test_aws_cli_is_in_path(host):  # noqa: D103
+    cmd = "aws --version"
     aws = host.run(cmd)
-    log.info('\n%s', aws.stderr)
-    assert host.exists('aws')
+    log.info("\n%s", aws.stderr)
+    assert host.exists("aws")
 
 
-def test_repo_access(host):
-    cmd = 'yum -y repolist all | sed -n \'/^repo id/,$p\''
+def test_repo_access(host):  # noqa: D103
+    cmd = "yum -y repolist all | sed -n '/^repo id/,$p'"
     repos = host.run(cmd)
-    log.info('stdout:\n%s', repos.stdout)
-    log.info('stderr:\n%s', repos.stderr)
+    log.info("stdout:\n%s", repos.stdout)
+    log.info("stderr:\n%s", repos.stderr)
     assert repos.exit_status == 0
     assert "Errno" not in repos.stderr
 
 
 @pytest.mark.el7
-def test_boot_is_mounted(host):
-    boot = host.mount_point('/boot')
+def test_boot_is_mounted(host):  # noqa: D103
+    boot = host.mount_point("/boot")
     assert boot.exists
 
 
-def test_tmp_mount_properties(host):
-    tmp = host.mount_point('/tmp')
+def test_tmp_mount_properties(host):  # noqa: D103
+    tmp = host.mount_point("/tmp")
     assert tmp.exists
-    assert tmp.device == 'tmpfs'
-    assert tmp.filesystem == 'tmpfs'
+    assert tmp.device == "tmpfs"
+    assert tmp.filesystem == "tmpfs"
 
 
-def test_el7_selinux_enforcing(host):
-    cmd = 'test $(getenforce) = \'Enforcing\''
+def test_el7_selinux_enforcing(host):  # noqa: D103
+    cmd = "test $(getenforce) = 'Enforcing'"
     selinux_permissive = host.run(cmd)
     assert selinux_permissive.exit_status == 0
 
 
 @pytest.mark.el7
 @pytest.mark.fips_enabled
-def test_el7_fips_enabled(host):
-    fips = host.file('/proc/sys/crypto/fips_enabled')
-    assert fips.exists and fips.content.strip() == b'1'
+def test_el7_fips_enabled(host):  # noqa: D103
+    fips = host.file("/proc/sys/crypto/fips_enabled")
+    assert fips.exists and fips.content.strip() == b"1"
 
 
 @pytest.mark.el7
 @pytest.mark.fips_disabled
-def test_el7_fips_disabled(host):
-    fips = host.file('/proc/sys/crypto/fips_enabled')
-    assert not fips.exists or fips.content.strip() == b'0'
+def test_el7_fips_disabled(host):  # noqa: D103
+    fips = host.file("/proc/sys/crypto/fips_enabled")
+    assert not fips.exists or fips.content.strip() == b"0"
 
 
-@pytest.mark.parametrize("names", [
-    ([
-        'python3',
-        'python36',
-    ])
-])
-def test_python3_installed(host, names):
-    pkg = type('pkg', (object,), {'is_installed': False})
+@pytest.mark.parametrize(
+    "names",
+    [
+        (
+            [
+                "python3",
+                "python36",
+            ]
+        )
+    ],
+)
+def test_python3_installed(host, names):  # noqa: D103
+    pkg = type("pkg", (object,), {"is_installed": False})
     for name in names:
         pkg = host.package(name)
         if pkg.is_installed:
             break
 
     assert pkg.is_installed
-    log.info(
-        '%s',
-        {'pkg': pkg.name, 'version': pkg.version, 'release': pkg.release})
+    log.info("%s", {"pkg": pkg.name, "version": pkg.version, "release": pkg.release})
 
 
-@pytest.mark.parametrize("realpaths,link", [
-    (('/usr/bin/python3.6', '/usr/libexec/platform-python3.6'), '/usr/bin/python3')
-])
-def test_python3_symlink(host, realpaths, link):
+@pytest.mark.parametrize(
+    "realpaths,link",
+    [(("/usr/bin/python3.6", "/usr/libexec/platform-python3.6"), "/usr/bin/python3")],
+)
+def test_python3_symlink(host, realpaths, link):  # noqa: D103
     python3_symlink = host.file(link).linked_to
     assert python3_symlink in realpaths
 
 
-@pytest.mark.parametrize("version", [
-    ('3.6')
-])
-def test_python3_version(host, version):
-    cmd = 'python3 --version'
+@pytest.mark.parametrize("version", [("3.6")])
+def test_python3_version(host, version):  # noqa: D103
+    cmd = "python3 --version"
     python3_version = host.run(cmd)
-    log.info('`%s` stdout: %s', cmd, python3_version.stdout)
-    log.info('`%s` stderr: %s', cmd, python3_version.stderr)
+    log.info("`%s` stdout: %s", cmd, python3_version.stdout)
+    log.info("`%s` stderr: %s", cmd, python3_version.stderr)
 
     assert python3_version.exit_status == 0
 
@@ -139,47 +145,53 @@ def test_python3_version(host, version):
     assert python3_version.stdout.strip().split()[1].startswith(version)
 
 
-def test_timedatectl_dbus_status(host):
-    cmd = 'timedatectl'
+def test_timedatectl_dbus_status(host):  # noqa: D103
+    cmd = "timedatectl"
     timedatectl = host.run(cmd)
-    log.info('stdout:\n%s', timedatectl.stdout)
-    log.info('stderr:\n%s', timedatectl.stderr)
+    log.info("stdout:\n%s", timedatectl.stdout)
+    log.info("stderr:\n%s", timedatectl.stderr)
     assert timedatectl.exit_status == 0
 
 
-def test_var_run_symlink(host):
-    var_run_symlink = host.file('/var/run').linked_to
-    assert var_run_symlink == '/run'
+def test_var_run_symlink(host):  # noqa: D103
+    var_run_symlink = host.file("/var/run").linked_to
+    assert var_run_symlink == "/run"
 
 
-@pytest.mark.parametrize("service", [
-    ("amazon-ssm-agent.service"),
-])
-def test_systemd_services(host, service):
+@pytest.mark.parametrize(
+    "service",
+    [
+        ("amazon-ssm-agent.service"),
+    ],
+)
+def test_systemd_services(host, service):  # noqa: D103
     chk_service = host.service(service)
     assert chk_service.is_enabled
 
 
-@pytest.mark.parametrize("name", [
-    ("spel-release"),
-    ("amazon-ssm-agent"),
-    ("ec2-hibinit-agent"),
-    ("ec2-instance-connect"),
-    ("ec2-net-utils"),
-])
-def test_spel_packages(host, name):
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("spel-release"),
+        ("amazon-ssm-agent"),
+        ("ec2-hibinit-agent"),
+        ("ec2-instance-connect"),
+        ("ec2-net-utils"),
+    ],
+)
+def test_spel_packages(host, name):  # noqa: D103
     pkg = host.package(name)
     if pkg.is_installed:
         log.info(
-            '%s',
-            {'pkg': pkg.name, 'version': pkg.version, 'release': pkg.release})
+            "%s", {"pkg": pkg.name, "version": pkg.version, "release": pkg.release}
+        )
     assert pkg.is_installed
 
 
-def test_cfn_bootstrap(host):
-    cmd = 'python3 -m pip show aws-cfn-bootstrap'
+def test_cfn_bootstrap(host):  # noqa: D103
+    cmd = "python3 -m pip show aws-cfn-bootstrap"
     cfnbootstrap = host.run(cmd)
-    log.info('stdout:\n%s', cfnbootstrap.stdout)
-    log.info('stderr:\n%s', cfnbootstrap.stderr)
+    log.info("stdout:\n%s", cfnbootstrap.stdout)
+    log.info("stderr:\n%s", cfnbootstrap.stderr)
     assert cfnbootstrap.exit_status == 0
-    assert 'Version: 2.0' in cfnbootstrap.stdout
+    assert "Version: 2.0" in cfnbootstrap.stdout
