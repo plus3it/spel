2ff8c2faa910c00fc525cf4de27b7819832d6c80
Thomas H Jones II <github@xanthia.com>
2024-02-06 08:11:52 -0500

    Make output-suppression expression more terse

--- tests/scripts/grow_check.sh
+++ tests/scripts/grow_check.sh
@@ -38,7 +38,8 @@ SEL_MODE="$( getenforce )"
 # Run the grow-part task
 [[ -d /sys/fs/selinux ]] && setenforce Permissive
 printf "Attempting to grow %s... " "${ROOT_DSKPRT:-}"
-growpart "${ROOT_DISK:-}" "${ROOT_PART:-}" > /dev/null 2&>1 \
-  || ( echo "FAILED" ; exit 1) \
-  && echo "Success!"
+growpart "${ROOT_DISK:-}" "${ROOT_PART:-}" &>/dev/null || (
+  echo "FAILED" ; exit 1
+)
+echo "Success!"
 [[ -d /sys/fs/selinux ]] && setenforce "${SEL_MODE:-}"
