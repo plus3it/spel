4ef75dc633c9b5d1b3f1dc37dc1a27ba2d5609e6
Thomas H Jones II <github@xanthia.com>
2024-02-15 08:03:54 -0500

    Make stopping of auditd service conditional
    Closes #665

--- spel/scripts/free-root.sh
+++ spel/scripts/free-root.sh
@@ -11,8 +11,14 @@ set -e
 echo "Restarting systemd"
 systemctl daemon-reexec
 
-echo "Killing auditd"
-service auditd stop
+# The auditd (UpStart) service may or may not be running...
+if [[ $( service auditd status > /dev/null 2>&1 )$? -eq 0 ]]
+then
+  echo "Killing auditd"
+  service auditd stop
+else
+  echo "The auditd service is not running"
+fi
 
 echo "Kill all non-essential services"
 for SERVICE in $(
